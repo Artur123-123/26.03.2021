3e71ca73b247c516553e41e53aa0be9d8b6908f9
Artur-123-123 <arturolejnik462@gmail.com>
2021-03-26 12:04:32 +0100

    26.03.2021

---
+++ main.js
@@ -0,0 +1,49 @@
+function formularz() {
+    const imie = document.getElementById('imie').value
+    console.log(imie)
+}
+
+function formularz2() {
+    const naz = document.getElementById('nazwisko').value
+    console.log(naz)
+}
+
+function formularz3() {
+    const mail = document.getElementById('email').value
+    console.log(mail)
+}
+
+function formularz4() {
+    const tel = document.getElementById('telefon').value
+    console.log(tel)
+}
+
+function formularz5() {
+    const mess = document.getElementById('mess').value
+    console.log(mess)
+}
+
+function onchange1() {
+    const imie = document.getElementById('imie').value
+    console.log(imie)
+}
+
+function onchange2() {
+    const naz = document.getElementById('nazwisko').value
+    console.log(naz)
+}
+
+function onchange3() {
+    const mail = document.getElementById('email').value
+    console.log(mail)
+}
+
+function onchange4() {
+    const tel = document.getElementById('telefon').value
+    console.log(tel)
+}
+
+function onchange5() {
+    const mess = document.getElementById('mess').value
+    console.log(mess)
+}
