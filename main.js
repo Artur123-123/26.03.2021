46e59989608aa48969e3fb7bf9a3b2ff1a5aaade
Artur-123-123 <arturolejnik462@gmail.com>
2021-03-26 15:21:46 +0100

    26.03.2021

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ function formularz2() {
 }
 
 function formularz3() {
-    const mail = document.getElementById('email').value
-    console.log(mail)
+    const mail = document.getElementById('email').value 
+        console.log(mail)
 }
 
 function formularz4() {
@@ -46,4 +46,59 @@ function onchange4() {
 function onchange5() {
     const mess = document.getElementById('mess').value
     console.log(mess)
+}
+
+function walidacja() {
+    const imie = document.getElementById('imie').value
+    if(imie.length < 3) {
+        alert ("Za krótkie imię")
+    }
+}
+
+function walidacjaNaz() {
+    const naz = document.getElementById('nazwisko').value
+    if(naz.length < 5) {
+        alert ("Za krótkie nazwisko")
+    }
+}
+
+function walidacjaWiad() {
+    const mess = document.getElementById('mess').value
+    if(mess.length < 20) {
+        alert ("Za krótka wiadomość")
+    }
+}
+
+// function walidacjaMail(mail) {
+//     let split1 = mail.split('')
+//     console.log(split1)
+//     split1.some(item => item === '@')
+// }
+
+function telefon() {
+    const tele = document.getElementById('telefon').value
+    if(tele !== Number) {
+        alert ('Tylko cyfry')
+    }
+}
+
+// function email() {
+//     const mail = document.getElementsByName('email').value
+
+// }
+
+function fon(val) {
+    const arr = val.split('')
+    const num = arr.filter(item => {
+        if(item === '1' || item === '2' || item === '3' || item === '4' || item === '5' || item === '6' || item === '7' || item === '8' || item === '9') {
+            return item
+        } else {
+             alert("Tylko cyfry ${item}")
+             return
+         }
+    })
+    console.log(num)
+    // const isNum = Number(telefon)
+    // const num = parseFloat(telefon)
+    // console.log(num)
 }
